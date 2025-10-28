59ab77387f010de4131b7902451475c6dc702bc0
Kareem Mohamed <kareem.mohamed@adfinis.com>
2025-10-28 22:20:05 +0300

    Task1

--- src/main.c
+++ src/main.c
@@ -8,3 +8,6 @@ int b = 5;
 printf("Sum  = %d + %d = %d, a, b, a + b); // Sum = 10 + 5 = 15
 
 return 0; }
+
+// here should some functions
+
--- src/test.py
+++ src/test.py
@@ -1,3 +1,10 @@
 list_Of_names = ["Ahmed","Ali","Alaa","Ashraf"]
 print(list_Of_names)
 print("Hello Session Three")
+
+
+def print_name(name):
+    print(f"Your name is {name}")
+
+
+print_name("Ahmed Alaa")
